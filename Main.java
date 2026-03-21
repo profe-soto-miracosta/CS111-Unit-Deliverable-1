e3db54b531eff834526fbc0201ea21447c1982d6
Raul Soto <rsoto@miracosta.edu>
2026-03-21 15:29:53 +0000

    Updated Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,19 @@
-/********************************************
-*	AUTHORS:	<your names>
-* COLLABORATORS: <name of peer, tutor, instructor, anyone else who helped>
-*	LAST MODIFIED:	<date of last change>
-********************************************/
-
-/********************************************
-*	<TITLE OF PROGRAM>
-*********************************************
-*	PROGRAM DESCRIPTION:
-*	<1-2 sentences describing overall program>
-*********************************************
-*	ALGORITHM:
-*	<Pseudocode here>
-*********************************************
-*	STATIC METHODS:
-* <list of static methods and which teammate made each>
-*********************************************/
-
+/**
+ *********************************************
+ *	<TITLE OF PROGRAM>
+ *********************************************
+ *	PROGRAM DESCRIPTION:
+ *	<1-2 sentences describing overall program>
+ *********************************************
+ *	ALGORITHM:
+ *	<Pseudocode here>
+ *********************************************
+ *	STATIC METHODS:
+ * <list of static methods and which teammate made each>
+ ********************************************
+ *
+ * @author Your Name Here
+ */
 
 public class Main 
 {
